09c5c12e9935a3340a3d8a637ebca1d3f2853c5a
Bhaskar <bhaskar950880@gmail.com>
2026-07-14 14:58:42 +0530

    14/07

---
+++ number1to0toallzero.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class number1to0toallzero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = sc.next();
+        int k = sc.nextInt();
+        for (int i = 0; i < k; i++) {
+            int count = 0;
+            for (int j = i; j < n; j += k) {
+                if (s.charAt(j) == '1') {
+                    count++;
+                }
+            }
+            if (count % 2 != 0) {
+                System.out.println("no");
+                break;
+            }
+            else{
+                System.out.println("yes");
+            }
+        }
+    }
+    
+}
