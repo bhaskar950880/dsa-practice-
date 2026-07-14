36911d57b5b42a265abb99adb764977f54736704
Bhaskar <bhaskar950880@gmail.com>
2026-07-14 14:47:36 +0530

    14/07

---
+++ reversedigita.java
@@ -0,0 +1,20 @@
+public class reversedigita {
+    public static void main(String[] args) {
+        int arr[] ={2,3,4,5,6};
+        rev(0,arr.length-1, arr);
+        for (int i = 0; i   < arr.length-1; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void rev(int left, int right , int [] arr){
+        if(left>right){
+            return;
+        }
+        else{
+            int a=   arr[left];
+            arr[left]=arr[right];
+            arr  [right]= a;
+        }
+        rev(left+1, right-1, arr);
+    }
+}
